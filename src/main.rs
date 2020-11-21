37b9304cbaf7beddb5ee97b8c44e25840ea85fe7
fadedbee <github@fadedbee.com>
2020-11-21 11:38:59 +0000

    refactored to allow re-used of state changes for final chunk size

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,73 @@ enum State {
     Different(u64)
 }
 
+/* 
+ * Appends "num" bytes at "offset" in src to dst.
+ */
+fn append_data(dst: &mut File, src: &mut File, num: u64, offset: u64) -> Result<()> {
+    const COPY_CHUNKSIZE: usize = 8;
+    const COPY_CHUNKLEN: u64 = COPY_CHUNKSIZE as u64;
+
+    let num_chunks = num / COPY_CHUNKLEN;
+    let remainder = num - num_chunks * COPY_CHUNKLEN;
+
+    src.seek(SeekFrom::Current(-((num + offset) as i64)))?;
+
+    let mut copybuf = vec![0u8; COPY_CHUNKSIZE];
+    for _ in 0..(num / COPY_CHUNKLEN) {
+        src.read(&mut copybuf)?;
+        dst.write(&copybuf)?;
+    }
+
+    let mut copybuf = vec![0u8; remainder as usize];
+    src.read(&mut copybuf)?;
+    dst.write(&copybuf)?;
+
+    src.seek(SeekFrom::Current(offset as i64))?;
+
+    Ok(())
+}
+
+fn next_state(state: State, ochunk: &mut Vec<u8>, nchunk: &mut Vec<u8>, new: &mut File, delta: &mut File, offset: u64, chunklen: u64) -> Result<State> {
+    Result::Ok(match state {
+        State::Init => {
+            if nchunk == ochunk {
+                delta.write(&[COPY])?;
+                State::Matching(chunklen)
+            } else {
+                delta.write(&[IMMEDIATE])?;
+                State::Different(chunklen)
+            }
+        },
+        State::Matching(num) => {
+            if nchunk == ochunk {
+                println!("0same: {:02X?} {:02X?}", ochunk, nchunk);
+                State::Matching(num + chunklen)
+            } else {
+                println!("0diff: {:02X?} {:02X?}", ochunk, nchunk);
+                delta.write(&u64tou8ale(num))?;
+                delta.write(&[IMMEDIATE])?;
+                State::Different(chunklen)
+            }
+        },
+        State::Different(num) => {
+            if nchunk == ochunk {
+                println!("1same: {:02X?} {:02X?}", ochunk, nchunk);
+                delta.write(&u64tou8ale(num))?;
+                
+                // append data from new to delta
+                append_data(delta, new, num, chunklen)?;
+
+                delta.write(&[COPY])?;
+                State::Matching(chunklen)
+            } else {
+                println!("1diff: {:02X?} {:02X?}", ochunk, nchunk);
+                State::Different(num + chunklen)
+            }
+        }
+    })
+}
+
 fn main() -> Result<()> {
 	let args = Cli::from_args();
 
@@ -51,56 +118,27 @@ fn main() -> Result<()> {
 
     let mut state = State::Init;
 
-    let mut ochunk = [0; CHUNKSIZE];
-    let mut nchunk = [0; CHUNKSIZE];
-    let mut copybuf = [0; CHUNKSIZE];
+    let mut ochunk = vec![0u8; CHUNKSIZE];
+    let mut nchunk = vec![0u8; CHUNKSIZE];
     let num_chunks = min_len / CHUNKLEN;
+    let remainder = min_len - num_chunks * CHUNKLEN;
+    println!("remainder: {:?}", remainder);
+
+    // process all of the whole chunks
     for cnum in 0..num_chunks {
         old.read(&mut ochunk)?;
         new.read(&mut nchunk)?;
-        state = match state {
-            State::Init => {
-                if nchunk == ochunk {
-                    delta.write(&[COPY])?;
-                    State::Matching(CHUNKLEN)
-                } else {
-                    delta.write(&[IMMEDIATE])?;
-                    State::Different(CHUNKLEN)
-                }
-            },
-            State::Matching(num) => {
-                if nchunk == ochunk {
-                    println!("0same: {:02X?} {:02X?}", ochunk, nchunk);
-                    State::Matching(num + CHUNKLEN)
-                } else {
-                    println!("0diff: {:02X?} {:02X?}", ochunk, nchunk);
-                    delta.write(&u64tou8ale(num))?;
-                    delta.write(&[IMMEDIATE])?;
-                    State::Different(CHUNKLEN)
-                }
-            },
-            State::Different(num) => {
-                if nchunk == ochunk {
-                    println!("1same: {:02X?} {:02X?}", ochunk, nchunk);
-                    delta.write(&u64tou8ale(num))?;
-                    
-                    // copy data
-                    new.seek(SeekFrom::Start(cnum * CHUNKLEN - num))?;
-                    for _ in 0..(num / CHUNKLEN) {
-                        new.read(&mut copybuf)?;
-                        delta.write(&copybuf)?;
-                    }
-                    new.seek(SeekFrom::Start((cnum + 1) * CHUNKLEN))?; // restore "new" seek position
-
-                    delta.write(&[COPY])?;
-                    State::Matching(CHUNKLEN)
-                } else {
-                    println!("1diff: {:02X?} {:02X?}", ochunk, nchunk);
-                    State::Different(num + CHUNKLEN)
-                }
-            }
-        }
+        state = next_state(state, &mut ochunk, &mut nchunk, &mut new, &mut delta, cnum * CHUNKLEN, CHUNKLEN)?;
     }
+
+    // process the final, partial chunk.
+    let mut partial_ochunk = vec![0u8; remainder as usize];
+    let mut partial_nchunk = vec![0u8; remainder as usize];
+    old.read(&mut partial_ochunk)?;
+    new.read(&mut partial_nchunk)?;
+    state = next_state(state, &mut partial_ochunk, &mut partial_nchunk, &mut new, &mut delta, num_chunks * CHUNKLEN, remainder)?;
+
+    // TODO: deal with the remaining bytes of *either* "old" or "new"
 
     // write final count
     match state {
@@ -113,18 +151,10 @@ fn main() -> Result<()> {
         State::Different(num) => {
             delta.write(&u64tou8ale(num))?;
 
-            // copy data
-            new.seek(SeekFrom::Start(num_chunks * CHUNKLEN - num))?;
-            for _ in 0..(num / CHUNKLEN) {
-                new.read(&mut copybuf)?;
-                delta.write(&copybuf)?;
-            }
-            new.seek(SeekFrom::Start((num_chunks + 1) * CHUNKLEN))?; // restore "new" seek position
-
+            // append data from new to delta
+            append_data(&mut delta, &mut new, num, 0)?;
         }
     }
-
-    println!("Hello, world2!");
 
 	Result::Ok(())
 }
